569383dcae8de65eb3321d74219310a7dae0e395
paolagan <paola.gandolfo@codingduck.it>
2023-09-11 17:19:08 +0200

    fine lezione

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 
-class Forma{
+abstract class Forma{
     static numeroDiOggettiCreati=0;
     private tipoForma:string="";
     public numeroFortunato:number = 10;
-    protected 
 
 
     constructor(tipo:string) {
@@ -15,77 +14,64 @@ class Forma{
         console.log("Il mio tipo e "+this.tipoForma);
     }
 
+    abstract circonferenza():number;
+
     printaTutto(){
         this.getTipoForma()
     }
-
 }
-
 
 
 
 class Triangolo extends Forma{
+//parametri della claseee Triangolo che definiscono i tre lati
+    a:number
+    b:number
+    c:number
 
-     pi: "prof"|"student" = "student"
-
-    // constructor() {
-    //     super("Triangolo");
-    // }
-
-    constructor(tipo:string) {
+    constructor(tipo:string, a:number,b:number,c:number) {
         super(tipo);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
     }
+
+    circonferenza():number{
+         return this.a+this.b+this.c;
+    }
+
+    printaTutto() {
+        super.printaTutto();
+        console.log("La circonferenza del triangolo e "+this.circonferenza())
+    }
+
+}
+
+let triangolo2 = new Triangolo("Trtiangolo vero 2",2,2,4)
+console.log("abbiamo creato # "+Forma.numeroDiOggettiCreati+" di oggetti.")
+console.log("abbiamo creato # "+Triangolo.numeroDiOggettiCreati+" di oggetti.")
+triangolo2.printaTutto()
+
+class Quadrato implements interface1{
+    a:number=0
+    nome:string=""
+
+    circonferenza(): number {
+        return 4*this.a;
+    }
+
+    getNome() {
+        return this.nome;
+    }
+
+}
+
+interface interface1{
+    nome:string;
+    getNome():string;
 }
 
 
-let triangolo = new Forma("triangolo");
-triangolo.getTipoForma()
-console.log("abbiamo creato # "+Forma.numeroDiOggettiCreati+" di oggetti.")
-
-let triangolo2 = new Triangolo("Trtiangolo vero 2")
-console.log("abbiamo creato # "+Forma.numeroDiOggettiCreati+" di oggetti.")
-console.log("abbiamo creato # "+Triangolo.numeroDiOggettiCreati+" di oggetti.")
-
-
-
-
-
-
-
-
-//
-//
-// class App{
-//
-//     private att1: string;
-//     private att2: string;
-//     private att3: string;
-//     private att4: string;
-//
-//     constructor(a: string ) {
-//        this.att1 = a;
-//     }
-//
-//     print () : string {
-//         return "ciccioprint";
-//     }
-//
-//     pippo () : number {
-//         return 1;
-//     }
-// }
-//
-//
-// class Child extends App{
-//
-//     print2():string{
-//         return super.print();
-//     }
-//
-//     pippo() : number{
-//         return 3;
-//     }
-// }
-//
-//
-// const app = new App("Oggetto 1")
+const test = new Quadrato()
+test.getNome()
