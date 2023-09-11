2037292d06ac363bbe762b410fc3fb74f2fb85f2
Guglielmo Frati <guglielmo.frati@codingduck.it>
2023-09-11 16:00:14 +0200

    Lezione 1

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,91 @@
 
-class App{
+class Forma{
+    static numeroDiOggettiCreati=0;
+    private tipoForma:string="";
+    public numeroFortunato:number = 10;
+    protected 
 
-    private att1: string;
-    private att2: string;
-    private att3: string;
-    private att4: string;
 
-    constructor(a: string ) {
-       this.att1 = a;
+    constructor(tipo:string) {
+        Forma.numeroDiOggettiCreati+=1
+        this.tipoForma=tipo;
     }
 
-    print () : string {
-        return "ciccioprint";
+    getTipoForma():void{
+        console.log("Il mio tipo e "+this.tipoForma);
     }
 
-    pippo () : number {
-        return 1;
+    printaTutto(){
+        this.getTipoForma()
+    }
+
+}
+
+
+
+
+class Triangolo extends Forma{
+
+     pi: "prof"|"student" = "student"
+
+    // constructor() {
+    //     super("Triangolo");
+    // }
+
+    constructor(tipo:string) {
+        super(tipo);
     }
 }
 
 
-class Child extends App{
+let triangolo = new Forma("triangolo");
+triangolo.getTipoForma()
+console.log("abbiamo creato # "+Forma.numeroDiOggettiCreati+" di oggetti.")
 
-    print2():string{
-        return super.print();
-    }
-
-    pippo() : number{
-        return 3;
-    }
-}
+let triangolo2 = new Triangolo("Trtiangolo vero 2")
+console.log("abbiamo creato # "+Forma.numeroDiOggettiCreati+" di oggetti.")
+console.log("abbiamo creato # "+Triangolo.numeroDiOggettiCreati+" di oggetti.")
 
 
-const app = new App("Oggetto 1")
+
+
+
+
+
+
+//
+//
+// class App{
+//
+//     private att1: string;
+//     private att2: string;
+//     private att3: string;
+//     private att4: string;
+//
+//     constructor(a: string ) {
+//        this.att1 = a;
+//     }
+//
+//     print () : string {
+//         return "ciccioprint";
+//     }
+//
+//     pippo () : number {
+//         return 1;
+//     }
+// }
+//
+//
+// class Child extends App{
+//
+//     print2():string{
+//         return super.print();
+//     }
+//
+//     pippo() : number{
+//         return 3;
+//     }
+// }
+//
+//
+// const app = new App("Oggetto 1")
